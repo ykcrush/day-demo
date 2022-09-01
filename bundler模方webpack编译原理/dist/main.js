b8ddfbd4c61a6f34f32e3e1c770a9c5500f27aa2
ykcrush <ykcrushonwr1123@163.com>
2022-09-01 17:04:11 +0800

    bundler提交

---
+++ bundler模方webpack编译原理/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("sayyangkang"),console.log("sayyangkang")})();
---
+++ bundler模方webpack编译原理/src/message.js
@@ -0,0 +1,4 @@
+import { word } from "./word.js";
+const message = `say${word}`
+console.log(message)
+export default message 
